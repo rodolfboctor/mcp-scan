cea95e287cb07505686545d56c7eb1e0a7f7b209
Abanoub Rodolf Boctor <abanoub.rodolf@gmail.com>
2026-03-23 02:36:46 -0400

    chore: add .gitignore

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'es2022',
+  clean: true,
+  dts: true,
+  minify: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+});
