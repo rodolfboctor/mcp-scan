3ddf8413486dc3b4a545d1abcc3139869d87b07f
Abanoub Rodolf Boctor <rodolf@thynkq.com>
2026-03-23 03:22:27 -0400

    chore: Add and configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      } 
+    }
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    }
+  }
+];
